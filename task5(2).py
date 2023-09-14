9f6e25368046a2f54f947c8bc046362bd23173ed
AKILA S <143704564+akilaselvi28@users.noreply.github.com>
2023-09-14 22:02:21 +0530

    Add files via upload

---
+++ task5(2).py
@@ -0,0 +1,8 @@
+a=int(input("Enter value a : "))
+b=int(input("Enter value b : "))
+if(a>b):
+    print("a is greater than b ")
+elif(a<b):
+    print("a is less than b ")  
+else:
+    print("a is equal to b")      
