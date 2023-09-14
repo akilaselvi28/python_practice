ad348df47c60134d6a56efd86edc4b4b23c19a5d
AKILA S <143704564+akilaselvi28@users.noreply.github.com>
2023-09-14 21:47:18 +0530

    Add files via upload

---
+++ task5(1).py
@@ -0,0 +1,8 @@
+my_age=int(input("Enter my age : "))
+your_age=int(input("Enter your age : "))
+if (my_age>your_age):
+    print("you are not older than me! ")
+else :
+    print("you are older than me!")
+differences=your_age - my_age
+print ("differences of both in years :", differences)          
